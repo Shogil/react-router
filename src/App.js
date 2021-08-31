ec81870a99c43ba08bb1212241c1786fd4ff3de5
shogil <shogilbabu@gmail.com>
2021-08-31 19:18:01 +0530

    update usestate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from "./Container/About"
 import Profile from "./Container/Profile"
+import {useState} from "react"
 
 function App() {
+  const [state, setState] = useState('')
   return (
    <div className="App">
-       <button>About</button>
-       <button>Profile</button>
+       <button onClick={() => setState('about')}>About</button>
+       <button onClick={() => setState('profile')}>Profile</button>
 
       <About></About>
        <Profile></Profile>
