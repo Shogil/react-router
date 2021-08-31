a7346783a4af666bbb1e25d5472edea1e5b8e175
shogil <shogilbabu@gmail.com>
2021-08-31 19:24:23 +0530

    update the if condition

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import {useState} from "react"
 
 function App() {
   const [state, setState] = useState('')
+  let component
+  if(state ==='about')
+  {
+    component =      <About></About>
+
+  }else if(state ==='profile'){
+    component = <Profile></Profile>
+  }
   return (
+    
    <div className="App">
        <button onClick={() => setState('about')}>About</button>
        <button onClick={() => setState('profile')}>Profile</button>
-
-      <About></About>
-       <Profile></Profile>
+       {component}
    </div>
   );
 }
